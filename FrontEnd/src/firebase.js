3208cc9803acd7560f895c890294f82b96b369fb
Chandan N <chandan2004.n@gmail.com>
2026-07-06 22:19:01 +0530

    SmartBite Ai version 1.0

--- FrontEnd/src/firebase.js
+++ FrontEnd/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNNM_hVFvAz-ajiPaMHOEIziK4UriaTJI",
-  authDomain: "smartbite-4eedd.firebaseapp.com",
-  projectId: "smartbite-4eedd",
-  storageBucket: "smartbite-4eedd.firebasestorage.app",
-  messagingSenderId: "541194192888",
-  appId: "1:541194192888:web:ff31213f3cbff6ef06f80d",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-4M6WP33X3V"
 };
 
